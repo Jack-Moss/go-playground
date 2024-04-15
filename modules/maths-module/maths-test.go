5ff129e1debfea8dd44066cd7fc4e116dd7edb92
Jack-Moss <Jack-Moss-Work@hotmail.co.uk>
2024-04-15 08:42:27 +0100

    adjusted name

--- modules/maths-module/maths-test.go
+++ modules/maths-module/maths-test.go
@@ -1,4 +1,4 @@
-package main
+package maths-module
 
 import (
 	"math"
--- modules/test-module/test-file.go
+++ modules/test-module/test-file.go
@@ -5,6 +5,8 @@ import (
 )
 
 func Calculate() string {
+	demo_float := 9.12332233
+	println(maths - module.Do_abs(demo_float))
 	get_quote := quote.Go()
 	return get_quote
 }
